60876c0718028a81120299b0447f753c728c3e5d
BzzzThe18th <69125495+BzzzThe18th@users.noreply.github.com>
2023-03-10 16:05:08 -0800

    funny jman mod moment
    no waeee

---
+++ AirBenderBlender.cs
@@ -0,0 +1,80 @@
+﻿using BepInEx;
+using UnityEngine.XR;
+using Utilla;
+
+namespace AntMonkePC
+{
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.6.7")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class AntPlugin : BaseUnityPlugin
+    {
+        bool inRoom;
+        VRRig r;
+        XRNode rNode = XRNode.RightHand;
+        XRNode lNode = XRNode.LeftHand;
+
+        void OnEnable()
+        {
+            GorillaLocomotion.Player.Instance.gameObject.GetComponent<SizeManager>().enabled = false;
+            HamburgerPickles.ExtraPickles();
+        }
+
+        void OnDisable()
+        {
+            GorillaLocomotion.Player.Instance.gameObject.GetComponent<SizeManager>().enabled = true;
+            HamburgerPickles.NoPicklesPls();
+        }
+
+        void FixedUpdate()
+        {
+            if (inRoom)
+            {
+                bool rSC;
+                InputDevices.GetDeviceAtXRNode(rNode).TryGetFeatureValue(CommonUsages.primary2DAxisClick, out rSC);
+                bool lSC;
+                InputDevices.GetDeviceAtXRNode(lNode).TryGetFeatureValue(CommonUsages.primary2DAxisClick, out lSC);
+                bool lG;
+                InputDevices.GetDeviceAtXRNode(lNode).TryGetFeatureValue(CommonUsages.gripButton, out lG);
+
+                if (r == null || r.photonView == null)
+                {
+                    VRRig[] rs = FindObjectsOfType<VRRig>();
+                    foreach (VRRig rig in rs)
+                    {
+                        if (rig.photonView != null && rig.photonView.IsMine) r = rig;
+                    }
+                }
+
+                if (rSC && !lG)
+                {
+                    GorillaLocomotion.Player.Instance.scale += GorillaLocomotion.Player.Instance.scale > 10f ? 0f : 0.01f;
+                    GorillaLocomotion.Player.Instance.gameObject.GetComponent<SizeManager>().enabled = false;
+                }
+                if (lSC && !lG)
+                {
+                    GorillaLocomotion.Player.Instance.scale -= GorillaLocomotion.Player.Instance.scale < 0.06f ? 0f : 0.01f;
+                    GorillaLocomotion.Player.Instance.gameObject.GetComponent<SizeManager>().enabled = false;
+                }
+                if (rSC && lG && !lSC)
+                { 
+                    GorillaLocomotion.Player.Instance.gameObject.GetComponent<SizeManager>().enabled = true;
+                }
+            }
+        }
+
+        [ModdedGamemodeJoin]
+        public void OnJoin(string gamemode)
+        {
+            inRoom = true;
+            GorillaLocomotion.Player.Instance.gameObject.GetComponent<SizeManager>().enabled = false;
+        }
+
+        [ModdedGamemodeLeave]
+        public void OnLeave(string gamemode)
+        {
+            inRoom = false;
+            GorillaLocomotion.Player.Instance.gameObject.GetComponent<SizeManager>().enabled = true;
+        }
+    }
+}
---
+++ HarmonyPatches.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using System;
+using System.Reflection;
+
+namespace AntMonkePC
+{
+    public class HamburgerPickles
+    {
+        private static Harmony instance;
+
+        public static bool IsPatched { get; private set; }
+        public const string InstanceId = PluginInfo.GUID;
+
+        internal static void ExtraPickles()
+        {
+            if (!IsPatched)
+            {
+                if (instance == null)
+                {
+                    instance = new Harmony(InstanceId);
+                }
+
+                instance.PatchAll(Assembly.GetExecutingAssembly());
+                IsPatched = true;
+            }
+        }
+
+        internal static void NoPicklesPls()
+        {
+            if (instance != null && IsPatched)
+            {
+                instance.UnpatchSelf();
+                IsPatched = false;
+            }
+        }
+    }
+}
